01c3fc1967086e81c130c3214c90fe3f29aab4a5
Tiffany Lin <lin.tiffc@gmail.com>
2017-04-17 19:33:42 -0400

    Add files via upload

---
+++ head.php
@@ -0,0 +1,39 @@
+<!doctype HTML>
+<html>
+<head>
+	<title>
+		ETHICAL COLLAB UW
+	</title>
+
+	<link rel="stylesheet" href="style.css" type="text/css">
+
+	<script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="js/imgfade.js" type="text/javascript"></script>
+	<script src="js/anchorscroll.js" type="text/javascript"></script>
+	<!--Box Slider-->
+	<script src="js/jquery.bxslider.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+  			$('.bxslider').bxSlider();
+		});
+	</script>
+	<link href="js/jquery.bxslider.css" rel="stylesheet" />
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,500,500italic,700,900' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<nav class="top"> 
+		<ul class="left">
+			<li><a href="index.php">Main</a></li>
+			<li><a href="research.php">Research</a></li>
+			<!--<li><a href="testimonies.php">Testimonies</a></li>-->
+			<li><a href="about.php">About</a></li>
+			<!--<li><a href="media.php">Media</a></li>-->
+			<li><a href="press.php">Press</a></li>
+		</ul>
+	</nav>
+
+	<header>
+		ETHICAL COLLAB UW
+	</header>
---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+
+<head>
+	<title>Tiff C. Lin</title>
+	
+	<meta name="description" content="" />
+	<meta name="author" content="Tiff C. Lin">
+	<meta name="keywords" content="tiffany lin, tiffany, lin, tiffclin, portfolio, design, research, wrting, art, internet, project, web, webdesign, webdesigner, designer, professional, academic, creation"/>
+	
+	<link rel="icon" type="image/png" href="logo.png">
+      
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+	
+	<script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="js/anchorscroll.js" type="text/javascript"></script>
+	
+</head>
+
+<body>
+	<section id="main">
+	<div id="maincontent">
+	<img src="images/logo.png" class="logo">
+	<div id="mainnav">
+		<ul>
+			<li><a href="#work">Work</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="resume.php">C.V./Resume</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li><a href="http://write.tiffclin.com">Blog</a></li>
+		</ul>
+	</div>
+	</div>
+	</section>
+	
+	<a name="about">
+	<section id="about">
+	<p>Hi, I'm Tiff. I'm a <a href="http://uwaterloo.ca/knowledge-integration">KI</a> undergrad student at the University of Waterloo (minor in Sociology, specialization in Collaborative Design). </p>
+	<p>My interests include design, communications, and research in technology and society (particularly Internet and society, security, and and culture). </p>
+	<p>In my spare time, I also love to do art, web and graphic design, and to write.</p>
+	<p>I'd love to hear from you if you're interested in collaborating on a project!</p>	 
+	</section>
+	
+	<a name="work">
+	<section>
+	<p>test</p>
+	</section>
+	
+	<footer>
+	</footer>
+</body>
+
+</html>
